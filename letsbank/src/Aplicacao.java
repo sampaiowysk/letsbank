b0f746ed197590607c448828064513186486c640
F3310098 Flavio Nuno Maia de Sousa Filho <flavio.sousa@bb.com.br>
2022-06-13 18:05:24 -0300

    Add interaction to main

--- letsbank/src/Aplicacao.java
+++ letsbank/src/Aplicacao.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Aplicacao {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo ao Let's Bank");
+
+        System.out.println("Escolha a operação:");
+        System.out.println("1 - Criar conta");
+
+        var scanner = new Scanner(System.in);
+        var operation = scanner.nextInt();
+        if (operation == 1) {
+            System.out.println("Conta criada com sucesso");
+        } else {
+            System.out.println("Operação inválida");
+        }
     }
 }
